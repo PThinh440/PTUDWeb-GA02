fe759f6a3f4fe01c6238ad7a57376748fcd8eec6
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-27 21:50:53 +0700

    add router

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
+
+const router = express.Router;
 const app = express();
 const port = 3000;
 const keySession = "440457";
