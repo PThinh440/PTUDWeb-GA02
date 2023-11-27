17e1f405d110351df7db7f41df633c644055cbb0
PThinh440 <npthinh212@clc.fitus.edu.vn>
2023-11-27 19:25:08 +0700

    add express-session

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const app = express();
 const port = 3000;
+const keySession = 440457;
 
 app.use(express.json());
+
+const store = session.MemoryStore();
+
+app.use(session({
+    saveUninitialized: false,
+    secret: keySession,
+    cookie: {
+        maxAge: 1000 * 10 // 1s * 10
+    },
+    store
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/status', (req, res) => {
     res.status(200).json({
